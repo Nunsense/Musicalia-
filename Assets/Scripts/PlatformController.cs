6a83c493aff6bb7858f780fb7e7c6bdf83711d3a
Iuval <iuvalgoldansky@gmail.com>
2015-07-18 13:55:58 -0300

    Initial commit.

---
+++ Assets/Scripts/PlatformController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlatformController : MonoBehaviour {
+	
+	int num;
+	int type;
+	int modifier;
+	
+	public void Initialize(int _num, int _type, int _modifier) {
+		num = _num;
+		type = _type;
+		modifier = _modifier;
+	}
+	
+	void Start () {
+	
+	}
+}
---
+++ Assets/Scripts/WorldController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WorldController : MonoBehaviour {
+	public GameObject platform;
+	
+	void Start () {
+		StartLevel("");
+	}
+	
+	void Update () {
+	
+	}
+	
+	List<PlatformController> LevelGenerator(string json) {
+		List<PlatformController> plataforms = new List<PlatformController>();
+		
+		for (int i = 0; i < 20; i ++) {
+			GameObject plataform = GameObject.Instantiate(platform);
+			PlatformController platforms = platform.GetComponent<PlatformController>();
+			platforms.Initialize(2, 2, 0);
+			
+			platform.transform.position = new Vector3(i * 1, Random.Range(-2, 2), 0);
+		}
+		
+		return plataforms;
+	}
+	
+	public void StartLevel(string json) {
+		LevelGenerator(json);
+	}
+}
