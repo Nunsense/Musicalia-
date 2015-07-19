c54ae42488fd481acdcd0efba8067aab2320a141
jjszolno <jjszolno@gmail.com>
2015-07-19 12:33:29 -0300

    Fixed Pentagram follow.

---
+++ Assets/Scripts/PentagramController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PentagramController : MonoBehaviour {
+
+
+	void Awake() {
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 PlayerPOS = GameObject.Find("Player").transform.transform.position;
+		Vector3 PentagramPOS = new Vector3(PlayerPOS.x, transform.position.y, transform.position.z);
+		transform.position = PentagramPOS;
+	}
+}
