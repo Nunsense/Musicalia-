d72f96c00fae54f9b4d2dd3ac0f94eb19ef08019
mojouy <santiago.mollajoli@neonroots.com>
2015-07-21 08:04:28 -0300

    Remove extra assets, organize project.

--- Assets/Scripts/WorldController.cs
+++ Assets/Scripts/WorldController.cs
@@ -47,76 +47,12 @@ public class WorldController : MonoBehaviour {
 		return colors[Random.Range(0, colors.Length)];
 	}
 	
-//	class Plato {
-//		public int Num { get; set; } 
-//		public int Type { get; set; }
-//		public int Modifier { get; set; }
-//		
-//		public Plato(int _num, int _type, int _modifier) {
-//			Num = _num;
-//			Type = _type;
-//			Modifier = _modifier;
-//		}
-//	}
-	
-//	List<PlatformController> LevelGenerator(string json) {
-//		plataforms = new List<PlatformController>();
-//		
-//		List<Plato> platos = new List<Plato>();
-//		for (int i = 0; i < 20; i ++) {
-//			int type = Random.Range(1, 4);
-//			int num = Random.Range(0, 2);
-//			int modifier = 0;
-//			if (type == 3) {
-//				modifier = 1;
-//			}
-//			platos.Add(new Plato(num, type, modifier));
-//			if (type == 3) {
-//				platos.Add(new Plato(0, 0, 0));
-//			}
-//		}
-//
-//		for (int i = 0; i < platos.Count; i ++) {
-//			Plato plato = platos[i];
-//			
-//			GameObject plataform = null;
-//			
-//			switch (plato.Type) {
-//			case 1 : {
-//				plataform = GameObject.Instantiate(groundNormal);
-//				break;	
-//			}
-//			case 2 : {
-//				plataform = GameObject.Instantiate(groundFall);
-//				break;	
-//			}
-//			case 3 : {
-//				plataform = GameObject.Instantiate(groundJump);
-//				break;	
-//			}
-//			}
-//			if (plataform) {
-//				PlatformController controller = plataform.GetComponent<PlatformController>();
-//				controller.Initialize(plato.Num, plato.Type, plato.Modifier, 0);
-//				plataform.transform.position = new Vector3(i * groundWidth, plato.Num * heightMultiplier, 0);
-//				plataforms.Add(controller);
-//			}
-//		}
-//		
-//		return plataforms;
-//	}
-//	
-//	public void StartLevel(string json) {
-//		LevelGenerator(json);
-//	}
-	
 	public void Reset() {
 		for (int i = 0; i < plataforms.Length; i ++) {
 			plataforms[i].Reset();
 		}
 		player.Reset();
 		coinsTaken = 0;
-		tintAnim.SetInteger("step", 0);
 	}
 	
 	public void AddCoin() {
@@ -124,12 +60,5 @@ public class WorldController : MonoBehaviour {
 		Debug.Log("Coins: " + coinsTaken);
 		
 		float advance = totalCoins / coinsTaken;
-		if (advance > 0.25f) {
-			tintAnim.SetInteger("step", 1);
-		} else if (advance > 0.55f) {
-			tintAnim.SetInteger("step", 2);
-		} else if (advance > 0.9f) {
-			tintAnim.SetInteger("step", 3);
-		}
 	}
 }
